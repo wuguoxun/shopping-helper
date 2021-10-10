14783472331f53eca23e14633a6d6f60cd1f5616
wuguoxun <william.wu487@gmail.com>
2021-10-09 23:10:26 -0700

    Updates

---
+++ precache-manifest.af753861f05da10b4f24fc5eb67c08ae.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ce83745c28fbb08b6b3daac6e6259e7",
+    "url": "/shopping-helper/index.html"
+  },
+  {
+    "revision": "c44751ee1c4669c8022c",
+    "url": "/shopping-helper/static/js/2.9ab67eac.chunk.js"
+  },
+  {
+    "revision": "2ba02ab500541695586d",
+    "url": "/shopping-helper/static/js/main.058abded.chunk.js"
+  },
+  {
+    "revision": "73587629bec00a7b350f",
+    "url": "/shopping-helper/static/js/runtime-main.853f74bc.js"
+  }
+]);
